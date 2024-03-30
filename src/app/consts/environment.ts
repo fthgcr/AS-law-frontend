cf0e28d30e552950518328e3f0d1eaa5397f658f
fgocer <fatihhgocer@gmail.com>
2024-03-30 14:20:02 +0300

    environment

--- src/app/consts/environment.ts
+++ src/app/consts/environment.ts
@@ -1,10 +1,11 @@
 export const environment = {
     production: false,
     name: "Dev",
-    apiURL : "http://localhost:8080",
+    apiURL : "https://as-law-backend.onrender.com",
     wsURL : "http://localhost:8080/ws"
 }
 
 
 
 //apiURL : "https://as-law-backend.onrender.com",
+//apiURL : "http://localhost:8080",
